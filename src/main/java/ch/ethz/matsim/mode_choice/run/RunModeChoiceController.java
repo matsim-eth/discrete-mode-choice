00092764f6a7b787f5956bdd833fb711a791aa99
Sebastian Hörl <hoerl.sebastian@gmail.com>
2017-09-04 15:53:04 +0200

    Setup MNL

--- src/main/java/ch/ethz/matsim/mode_choice/run/RunModeChoiceController.java
+++ src/main/java/ch/ethz/matsim/mode_choice/run/RunModeChoiceController.java
@@ -1,29 +1,59 @@
 package ch.ethz.matsim.mode_choice.run;
 
+import java.util.Arrays;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.core.scenario.ScenarioUtils;
 
+import ch.ethz.matsim.mode_choice.ModeChoiceModel;
+import ch.ethz.matsim.mode_choice.mnl.CrowflyModeChoiceAlternative;
+import ch.ethz.matsim.mode_choice.mnl.ModeChoiceMNL;
 import ch.ethz.matsim.mode_choice.replanning.ModeChoiceStrategy;
 import ch.ethz.matsim.mode_choice.selectors.OldPlanForRemovalSelector;
+import ch.ethz.matsim.sioux_falls.SiouxFallsUtils;
 
 public class RunModeChoiceController {
 
 	public static void main(String[] args) {
+		Config config = ConfigUtils.loadConfig(SiouxFallsUtils.getConfigURL());
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		Controler controler = new Controler(scenario);
 
-		Controler controler = new Controler (args[0]);
-		
-		//or would it be better to have it in a sturtuplistener and 
-		//then access it through the MatsimServices?? mb sep '17
-		controler.getStrategyManager().setPlanSelectorForRemoval(new OldPlanForRemovalSelector<>());
-				
-		controler.addOverridingModule( new AbstractModule() {
+		// Set up MNL
+
+		ModeChoiceMNL model = new ModeChoiceMNL(MatsimRandom.getRandom());
+
+		for (String mode : Arrays.asList("car", "pt", "walk")) {
+			model.addModeAlternative(mode,
+					new CrowflyModeChoiceAlternative(config.planCalcScore().getMarginalUtilityOfMoney(),
+							config.plansCalcRoute().getModeRoutingParams().get(mode),
+							config.planCalcScore().getModes().get(mode)));
+		}
+
+		controler.addOverridingModule(new AbstractModule() {
 			@Override
 			public void install() {
-				
-				this.addPlanStrategyBinding("ModeChoiceStrategy").toProvider( ModeChoiceStrategy.class ) ;
+				bind(ModeChoiceModel.class).toInstance(model);
 			}
 		});
-		
-		controler.run();		
+
+		// or would it be better to have it in a sturtuplistener and
+		// then access it through the MatsimServices?? mb sep '17
+		controler.getStrategyManager().setPlanSelectorForRemoval(new OldPlanForRemovalSelector<>());
+
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+
+				this.addPlanStrategyBinding("ModeChoiceStrategy").toProvider(ModeChoiceStrategy.class);
+			}
+		});
+
+		controler.run();
 	}
 }
