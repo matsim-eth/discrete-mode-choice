b053775572cf1140862a1dcd9aaafa9b741a01e1
Sebastian Hörl <hoerl.sebastian@gmail.com>
2017-09-07 21:12:13 +0200

    Added estimated choice alternatives

---
+++ src/main/java/ch/ethz/matsim/mode_choice/scoring/mnl/PTChoiceAlternative.java
@@ -0,0 +1,38 @@
+package ch.ethz.matsim.mode_choice.scoring.mnl;
+
+import ch.ethz.matsim.mode_choice.ModeChoiceTrip;
+import ch.ethz.matsim.mode_choice.mnl.ModeChoiceAlternative;
+import ch.ethz.matsim.mode_choice.mnl.prediction.PredictionCache;
+import ch.ethz.matsim.mode_choice.mnl.prediction.PublicTransitPredictor;
+import ch.ethz.matsim.mode_choice.mnl.prediction.PublicTransitTripPrediction;
+import ch.ethz.matsim.mode_choice.scoring.ScoringModes;
+
+public class PTChoiceAlternative implements ModeChoiceAlternative {
+	final private PublicTransitPredictor predictor;
+	final private PredictionCache cache;
+	final private ScoringModes scoring;
+
+	public PTChoiceAlternative(ScoringModes scoring, PublicTransitPredictor predictor, PredictionCache cache) {
+		this.predictor = predictor;
+		this.cache = cache;
+		this.scoring = scoring;
+	}
+
+	@Override
+	public double estimateUtility(ModeChoiceTrip trip) {
+		PublicTransitTripPrediction prediction = (PublicTransitTripPrediction) cache.get(trip);
+
+		if (prediction == null) {
+			prediction = (PublicTransitTripPrediction) predictor.predictTrip(trip);
+			cache.put(trip, prediction);
+		}
+
+		return scoring.scorePtTrip(prediction.getPredictedTravelDistance(), prediction.getPredictedTravelTime(),
+				prediction.getNumberOfLineSwitches(), trip.getPerson());
+	}
+
+	@Override
+	public boolean isChainMode() {
+		return false;
+	}
+}
