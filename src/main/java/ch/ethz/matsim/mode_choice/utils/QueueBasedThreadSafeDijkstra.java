c638c06657a5a571f6c7ca02e8e1210d5897cb2b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-04-09 13:58:41 +0200

    Use IDs instead of Link objects

--- src/main/java/ch/ethz/matsim/mode_choice/utils/QueueBasedThreadSafeDijkstra.java
+++ src/main/java/ch/ethz/matsim/mode_choice/utils/QueueBasedThreadSafeDijkstra.java
@@ -12,7 +12,6 @@ import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.router.Dijkstra;
 import org.matsim.core.router.DijkstraFactory;
-import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelDisutility;
 import org.matsim.core.router.util.TravelTime;
 import org.matsim.vehicles.Vehicle;
@@ -21,8 +20,11 @@ public class QueueBasedThreadSafeDijkstra implements ThreadSafeLeastCostPathCalc
 	final private ExecutorService executor;
 	final private BlockingQueue<Dijkstra> instanceQueue = new LinkedBlockingQueue<>();
 
+	final private Network network;
+
 	public QueueBasedThreadSafeDijkstra(int numberOfInstances, Network network, TravelDisutility travelDisutility,
 			TravelTime travelTime) {
+		this.network = network;
 		executor = Executors.newFixedThreadPool(numberOfInstances);
 
 		try {
@@ -34,7 +36,7 @@ public class QueueBasedThreadSafeDijkstra implements ThreadSafeLeastCostPathCalc
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void close() {
 		executor.shutdownNow();
 	}
@@ -44,13 +46,25 @@ public class QueueBasedThreadSafeDijkstra implements ThreadSafeLeastCostPathCalc
 		if (executor.isShutdown()) {
 			throw new IllegalStateException();
 		}
-		
+
+		boolean fromNodeIdNotFound = !network.getNodes().containsKey(fromNode.getId());
+		boolean toNodeIdNotFound = !network.getNodes().containsKey(toNode.getId());
+
+		if (fromNodeIdNotFound || toNodeIdNotFound) {
+			throw new IllegalStateException();
+		}
+
 		try {
 			return executor.submit(new Callable<Path>() {
 				@Override
 				public Path call() throws Exception {
 					Dijkstra instance = instanceQueue.take();
-					Path result = instance.calcLeastCostPath(fromNode, toNode, starttime, person, vehicle);
+
+					// Recover the nodes here, because they may be different from the network that
+					// is used for routing (full network vs. road network)
+					Path result = instance.calcLeastCostPath(network.getNodes().get(fromNode.getId()),
+							network.getNodes().get(toNode.getId()), starttime, person, vehicle);
+
 					instanceQueue.put(instance);
 					return result;
 				}
