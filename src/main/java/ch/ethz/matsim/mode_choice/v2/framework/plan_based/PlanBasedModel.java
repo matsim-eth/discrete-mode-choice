15e35ae5d23a4eab4e5a5831dd425f8d5cb09e73
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-07-02 19:29:51 +0200

    update plans as tours

--- src/main/java/ch/ethz/matsim/mode_choice/v2/framework/plan_based/PlanBasedModel.java
+++ src/main/java/ch/ethz/matsim/mode_choice/v2/framework/plan_based/PlanBasedModel.java
@@ -2,7 +2,6 @@ package ch.ethz.matsim.mode_choice.v2.framework.plan_based;
 
 import ch.ethz.matsim.mode_choice.v2.framework.ModeAvailability;
 import ch.ethz.matsim.mode_choice.v2.framework.tour_based.TourBasedModel;
-import ch.ethz.matsim.mode_choice.v2.framework.tour_based.TourFinder;
 import ch.ethz.matsim.mode_choice.v2.framework.tour_based.constraints.TourConstraintFactory;
 import ch.ethz.matsim.mode_choice.v2.framework.tour_based.estimation.TourCandidate;
 import ch.ethz.matsim.mode_choice.v2.framework.tour_based.estimation.TourEstimator;
@@ -11,8 +10,7 @@ import ch.ethz.matsim.mode_choice.v2.framework.utils.ModeChainGeneratorFactory;
 
 public class PlanBasedModel extends TourBasedModel {
 	public PlanBasedModel(TourEstimator estimator, ModeAvailability modeAvailability,
-			TourConstraintFactory constraintFactory, TourFinder tourFinder,
-			UtilitySelectorFactory<TourCandidate> selectorFactory,
+			TourConstraintFactory constraintFactory, UtilitySelectorFactory<TourCandidate> selectorFactory,
 			ModeChainGeneratorFactory modeChainGeneratorFactory) {
 		super(estimator, modeAvailability, constraintFactory, new PlanTourFinder(), selectorFactory,
 				modeChainGeneratorFactory);
