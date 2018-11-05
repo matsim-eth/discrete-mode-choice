c7355c62cd1ace4875f7aea8dfb8d9354808bb2d
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-11-05 17:50:54 +0100

    Compatibility with MATSim 0.10.0

---
+++ src/main/java/ch/ethz/matsim/mode_choice/replanning/NonSelectedPlanSelector.java
@@ -0,0 +1,18 @@
+package ch.ethz.matsim.mode_choice.replanning;
+
+import org.matsim.api.core.v01.population.HasPlansAndId;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.core.replanning.selectors.PlanSelector;
+
+public class NonSelectedPlanSelector implements PlanSelector<Plan, Person> {
+	@Override
+	public Plan selectPlan(HasPlansAndId<Plan, Person> member) {
+		for (Plan plan : member.getPlans()) {
+			if (!plan.equals(member.getSelectedPlan())) {
+				return plan;
+			}
+		}
+		return null;
+	}
+}
