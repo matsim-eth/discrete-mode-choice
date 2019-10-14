f3edd830d46fa77921f84cd1b9450e3d6252f8f5
Sebastian Hörl <sebastian.hoerl@ivt.baug.ethz.ch>
2019-10-14 14:59:31 +0200

    Fix duplicate config parametersets

--- src/test/java/ch/ethz/matsim/discrete_mode_choice/modules/config/ConfigTest.java
+++ src/test/java/ch/ethz/matsim/discrete_mode_choice/modules/config/ConfigTest.java
@@ -1,9 +1,15 @@
 package ch.ethz.matsim.discrete_mode_choice.modules.config;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
@@ -29,5 +35,42 @@ public class ConfigTest {
 		assertEquals("unknown selector", dmcConfig2.getSelector());
 		assertEquals(new HashSet<>(dmcConfig.getCarModeAvailabilityConfig().getAvailableModes()),
 				new HashSet<>(Arrays.asList("abc", "def")));
+	}
+
+	@Test
+	public void testReadWriteConfigMultipleTimes() throws IOException {
+		DiscreteModeChoiceConfigGroup dmcConfig = new DiscreteModeChoiceConfigGroup();
+		Config config1 = ConfigUtils.createConfig(dmcConfig);
+
+		new ConfigWriter(config1).write("test_config1.xml");
+
+		Config config2 = ConfigUtils.loadConfig("test_config1.xml", new DiscreteModeChoiceConfigGroup());
+		new ConfigWriter(config2).write("test_config2.xml");
+
+		Config config3 = ConfigUtils.loadConfig("test_config2.xml", new DiscreteModeChoiceConfigGroup());
+		new ConfigWriter(config3).write("test_config3.xml");
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(new File("test_config3.xml"))));
+
+		String line = null;
+
+		int numberOfMultinomialLogit = 0;
+		int numberOfVehicleContinuity = 0;
+
+		while ((line = reader.readLine()) != null) {
+			if (line.contains("parameterset") && line.contains("selector:MultinomialLogit")) {
+				numberOfMultinomialLogit++;
+			}
+
+			if (line.contains("parameterset") && line.contains("tourConstraint:VehicleContinuity")) {
+				numberOfVehicleContinuity++;
+			}
+		}
+
+		reader.close();
+
+		assertEquals(1, numberOfMultinomialLogit);
+		assertEquals(1, numberOfVehicleContinuity);
 	}
 }
